01453ad7ed1c4402f8c9144e934ab3f0c9caa3bb
josh-mccullough <jam0005@hotmail.com>
2017-03-20 22:52:27 +0000

    Lives text added

---
+++ Assets/Scripts/Lives.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Lives : MonoBehaviour {
+    private int lives = 5;
+    public Text livesText; 
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        livesText.text = "Lives: " + lives.ToString();
+	}
+}
