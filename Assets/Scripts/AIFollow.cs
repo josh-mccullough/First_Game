c3db6bc6ceab2efa09b3e28e36b88d4a79d8e5bb
Josh Mccullough (MCC40013642) <MCC40013642@serc.ac.uk>
2017-03-20 14:56:56 +0000

    Added some 3D stuff and timer

---
+++ Assets/Scripts/AIFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIFollow : MonoBehaviour {
+
+    //You may consider adding a rigid body to the zombie for accurate physics simulation
+    private GameObject wayPoint;
+    private Vector3 wayPointPos;
+    //This will be the zombie's speed. Adjust as necessary.
+    private float speed = 6.0f;
+    void Start()
+    {
+        //At the start of the game, the zombies will find the gameobject called wayPoint.
+        wayPoint = GameObject.Find("wayPoint");
+    }
+
+    void Update()
+    {
+        wayPointPos = new Vector3(wayPoint.transform.position.x, transform.position.y, wayPoint.transform.position.z);
+        //Here, the zombie's will follow the waypoint.
+        transform.position = Vector3.MoveTowards(transform.position, wayPointPos, speed * Time.deltaTime);
+    }
+}
