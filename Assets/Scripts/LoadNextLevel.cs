96bd8635a51eeac42134a6dca8aea54b0253caad
josh-mccullough <jam0005@hotmail.com>
2017-03-23 16:09:04 +0000

    added nav mesh to 2nd level

--- Assets/Scripts/LoadNextLevel.cs
+++ Assets/Scripts/LoadNextLevel.cs
@@ -2,9 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LoadNextLevel : MonoBehaviour {
     public string level;
+
+  
+
+
 	void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
