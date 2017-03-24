407ac5056c7a23793f35d95a081a97f6e9fc13dd
josh-mccullough <jam0005@hotmail.com>
2017-03-24 13:45:10 +0000

    Second level complete

---
+++ Assets/Scripts/OrangeActivated.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrangeActivated : MonoBehaviour {
+    public bool OrangeActivation = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "SpecialCubes" && other.name == "Orange")
+        {
+            OrangeActivation = true;
+           
+        }
+
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        OrangeActivation = false;
+        
+
+    }
+}
---
+++ Assets/Scripts/TeleporterActivate.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleporterActivate : MonoBehaviour {
+    GameObject Blue;
+    GameObject Orange;
+    GameObject Player;
+    bool TeleporterReady = false;
+    bool MethodRan = false;
+
+
+    // Use this for initialization
+    void Start () {
+        gameObject.GetComponent<MeshRenderer>().enabled = false;
+        Blue = GameObject.Find("BlueTrigger");
+        Orange = GameObject.Find("OrangeTrigger");
+        Player = GameObject.Find("Character");
+        
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Blue.GetComponent<BlueActivated>().BlueActivation == true && Orange.GetComponent<OrangeActivated>().OrangeActivation == true && MethodRan == false)
+        {
+            ActivateTeleporter(); //doing this so that the mesh doesn't render every frame
+
+        }
+        else if ((Blue.GetComponent<BlueActivated>().BlueActivation == true || Orange.GetComponent<OrangeActivated>().OrangeActivation == true) )
+        {
+            gameObject.GetComponent<MeshRenderer>().enabled = false;
+        }
+	}
+
+    void ActivateTeleporter()
+    {
+        gameObject.GetComponent<MeshRenderer>().enabled = true;
+        TeleporterReady = true;
+        //TODO play a sound clip here maybe??
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player" && TeleporterReady == true)
+        {
+            Player.GetComponent<Transform>().position = new Vector3(3,66,9 );
+            //maybe play a clip here?
+        }
+
+    }
+}
